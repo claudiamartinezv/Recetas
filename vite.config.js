9ca5c304df0418c88a7d1c55e079837574b66f72
claudiamartinezv <77359482+claudiamartinezv@users.noreply.github.com>
2025-07-14 23:20:00 -0400

    Actualización

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Recetas/',
   plugins: [react()],
-  base: '/Recetas/' 
-})
-
+});
